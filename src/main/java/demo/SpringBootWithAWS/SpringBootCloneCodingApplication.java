7166818ca30a7bb847cba53025b07977bbaeff23
songhae <boricocke@naver.com>
2025-01-10 20:35:17 +0900

    Initial commit

---
+++ src/main/java/demo/SpringBootWithAWS/SpringBootCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package demo.SpringBootWithAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloneCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloneCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/SpringBootWithAWS/SpringBootCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.SpringBootWithAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloneCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
